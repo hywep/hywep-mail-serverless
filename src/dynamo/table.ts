e4a1fdc5af37c772793acb783ba0ff010b894028
boms03 <dvboms@gmail.com>
2025-01-12 20:00:41 +0900

    feat: edit table names

--- src/dynamo/table.ts
+++ src/dynamo/table.ts
@@ -1,6 +1,6 @@
 const addPrefix = (name: string) => `${name}-${process.env.NODE_ENV || 'dev'}`;
 
 export const DYNAMO_TABLE = Object.freeze({
-  RECRUIT: addPrefix('recruit'),
-  USERS: addPrefix('users'),
+  RECRUIT: addPrefix('hywep-recruit'),
+  USERS: addPrefix('hywep-users'),
 });
