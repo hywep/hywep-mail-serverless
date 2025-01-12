d4a8c207b5829a543ab87cc6faff230de187f43b
boms03 <dvboms@gmail.com>
2025-01-12 17:52:08 +0900

    feat: add getAllUsers function to fetch all DynamoDB users
    - Retrieve all user records from the DynamoDB USERS table.
    - Used ScanCommand with pagination to handle large datasets.
    - Applied unmarshall to convert DynamoDB items to JSON format.

---
+++ src/dynamo/dynamo.ts
@@ -0,0 +1,31 @@
+import { DynamoDBClient, ScanCommand } from '@aws-sdk/client-dynamodb';
+import { DYNAMO_TABLE } from './table';
+import { unmarshall } from '@aws-sdk/util-dynamodb';
+
+const dynamodb = new DynamoDBClient({ region: 'ap-northeast-2' });
+
+export async function getAllUsers(): Promise<any[]> {
+  const allUsers: any[] = [];
+  let lastEvaluatedKey: Record<string, any> | undefined = undefined;
+
+  while (true) {
+    const response = await dynamodb.send(
+      new ScanCommand({
+        TableName: DYNAMO_TABLE.USERS,
+        ExclusiveStartKey: lastEvaluatedKey,
+      }),
+    );
+
+    if (response.Items) {
+      allUsers.push(...response.Items.map((item) => unmarshall(item)));
+    }
+
+    if (!response.LastEvaluatedKey) {
+      break;
+    }
+
+    lastEvaluatedKey = response.LastEvaluatedKey;
+  }
+
+  return allUsers;
+}
